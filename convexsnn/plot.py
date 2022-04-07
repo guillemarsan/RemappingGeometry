266cb372245ae14564a94fb4677cb0be33c51f42
Guillermo Martín Sánchez <guillemartinsan@gmail.com>
2022-04-07 12:52:44 +0100

    Added plot support for more neurons

--- convexsnn/plot.py
+++ convexsnn/plot.py
@@ -38,14 +38,14 @@ def plot_1dbbox(x, y, F, G, T, basepath, plotx = False, foranim=False):
     for i in range(n):
         line = line_func(i,lim)
         if foranim and y2 == line_func(i,y1):
-            plt.plot(lim, line, color=c[i], linewidth = 10)
+            plt.plot(lim, line, color=c[i%10], linewidth = 10)
         else:
-            plt.plot(lim, line, color=c[i])
+            plt.plot(lim, line, color=c[i%10])
         if not foranim:
             px,py = np.meshgrid(lim,lim)
             plt.quiver(0, line_func(i,0), G[i,0],G[i,1],width=0.005,headwidth=5)
             plt.imshow((np.sign(G[i,1])*py > np.sign(G[i,1])*line_func(i,px)).astype(int), extent=(px.min(),px.max(),py.min(),py.max()),
-                cmap=colors.ListedColormap([c[i], 'white']), aspect='auto', origin="lower", alpha=0.3)
+                cmap=colors.ListedColormap([c[i%10], 'white']), aspect='auto', origin="lower", alpha=0.3)
         
     plt.xlabel('y1')
     plt.ylabel('y2')
@@ -69,9 +69,8 @@ def plot_2dbboxproj(x, y, F, G, T, basepath, plotx = False):
     c = plt.rcParams['axes.prop_cycle'].by_key()['color']
     y1 = y[0,:]
     y2 = y[1,:]
-    y3 = y[2,:]
-    lmin = np.min(y)-1
-    lmax = np.max(y)+1
+    lmin = np.min(y[:2,:])-1
+    lmax = np.max(y[:2,:])+1
     lim = np.linspace(lmin, lmax, 500)
     lim1, lim2 = np.meshgrid(lim,lim)
     plane_func = lambda i, pxs, pys: (F[i,:] @ x - T[i] - G[i,0] * pxs - G[i,1]*pys)/G[i,2]
@@ -83,7 +82,7 @@ def plot_2dbboxproj(x, y, F, G, T, basepath, plotx = False):
     maxidx = np.argmax(planes,axis=2)
     for i in range(n):
         plt.imshow((maxidx == i).astype(int), extent=(lmin,lmax,lmin,lmax),
-                cmap=colors.ListedColormap([c[i]]), aspect='auto', origin="lower", alpha=(maxidx==i).astype(float)*0.75)
+                cmap=colors.ListedColormap([c[i%10]]), aspect='auto', origin="lower", alpha=(maxidx==i).astype(float)*0.75)
     plt.xlabel('y1')
     plt.ylabel('y2')
     plt.xlim(lmin,lmax)
@@ -125,9 +124,9 @@ def plot_2dbbox(x, y, F, G, T, basepath, plotx = False, foranim=False):
         planes[mins[:,0],mins[:,1],i] = np.nan
 
         if foranim and y3 == plane_func(i,y1,y2):
-            ax3.plot_surface(X=lim1, Y=lim2, Z=planes[:,:,i], color=c[i])
+            ax3.plot_surface(X=lim1, Y=lim2, Z=planes[:,:,i], color=c[i%10])
         else:
-            ax3.plot_surface(X=lim1, Y=lim2, Z=planes[:,:,i], color=c[i], alpha=0.3)
+            ax3.plot_surface(X=lim1, Y=lim2, Z=planes[:,:,i], color=c[i%10], alpha=0.3)
 
     plt.xlabel('y1')
     plt.ylabel('y2')
@@ -185,13 +184,12 @@ def plot_neuroscience(x, y, V, s, t, basepath):
 
     ax2 = plt.subplot(2, 1, 2)
     maxV = np.max(V)
-    minV = np.min(V)
     c = plt.rcParams['axes.prop_cycle'].by_key()['color']
     l = 0.3
     off = 0.1
     for i in range(V.shape[0]):
-        ax2.plot(t,V[i,:], label='V' + str(i), color=c[i])
-        ax2.vlines(t[s[i,:] == 1], maxV + (i+1)*off + i*l, maxV + (i+1)*off + (i+1)*l, color=c[i])
+        ax2.plot(t,V[i,:], label='V' + str(i), color=c[i%10])
+        ax2.vlines(t[s[i,:] == 1], maxV + (i+1)*off + i*l, maxV + (i+1)*off + (i+1)*l, color=c[i%10])
     ax2.legend()
     ax2.set_ylabel('V(mV)')
     ax2.set_xlabel('t(ms)')
@@ -212,7 +210,7 @@ def plot_1drfs(p, r, dt, basepath, pad=0):
     filter = np.ones(m)*1/m
     for i in range(r.shape[0]):
         rf = np.convolve(r[i,:],filter, 'same')
-        plt.plot(p, rf, label='r' + str(i), color=c[i])
+        plt.plot(p, rf, label='r' + str(i), color=c[i%10])
     plt.legend()
     plt.ylabel('r')
     plt.xlabel('p')
@@ -246,6 +244,7 @@ def plot_1dspikebins(p, s, b, basepath, pad=0):
         plt.bar(ppts[:,0], sums)
         plt.title('Neuron %i' % i)
 
+    plt.tight_layout()
     filepath = "%s-1dspikebins.png" % basepath
     plt.savefig(filepath, dpi=600, bbox_inches='tight')
 
@@ -257,7 +256,7 @@ def plot_2drfs(p, r, dt, basepath):
     fig1 = plt.figure(figsize=(10,10))
     c = np.array(plt.rcParams['axes.prop_cycle'].by_key()['color'])
     maxs = np.max(r, axis=0)
-    colors = c[np.argmax(r, axis=0)]
+    colors = c[np.argmax(r, axis=0)%10]
     colors[maxs == 0] = 'k'
     plt.scatter(p[0,:], p[1,:], color=colors)
     plt.ylabel('p2')
@@ -267,16 +266,18 @@ def plot_2drfs(p, r, dt, basepath):
     plt.savefig(filepath, dpi=600, bbox_inches='tight')
 
     fig2 = plt.figure()
-    nrows = int(np.ceil(n/3))
+    ncols = int(np.sqrt(n))
+    nrows = int(np.ceil(n/ncols))
     tf = 0.2
     m = int(tf/dt)
     filter = np.ones(m)*1/m
     for i in np.arange(n):
-        plt.subplot(nrows, 3, i+1)
+        plt.subplot(nrows, ncols, i+1)
         rf = np.convolve(r[i,:],filter,'same')
         plt.scatter(p[0,:],p[1,:],c=rf, cmap='jet',s=1)
         plt.title('Neuron %i' % i)
 
+    plt.tight_layout()
     filepath = "%s-2drfs.png" % basepath
     plt.savefig(filepath, dpi=600, bbox_inches='tight')
 
@@ -299,9 +300,10 @@ def plot_2dspikebins(p, s, b, basepath, grid=True):
         pts = np.arange(b)
         ppts = p[:,pts*binsize]
     
-    nrows = int(np.ceil(n/3))
+    ncols = int(np.sqrt(n))
+    nrows = int(np.ceil(n/ncols))
     for i in np.arange(n):
-        plt.subplot(nrows, 3, i+1)
+        plt.subplot(nrows, ncols, i+1)
         sums = np.zeros(pts.shape[0])
         for j in pts:
             if grid:
@@ -316,6 +318,7 @@ def plot_2dspikebins(p, s, b, basepath, grid=True):
             plt.scatter(ppts[0,:],ppts[1,:],c=sums, cmap='jet')
         plt.title('Neuron %i' % i)
 
+    plt.tight_layout()
     filepath = "%s-2dspikebins.png" % basepath
     plt.savefig(filepath, dpi=600, bbox_inches='tight')
 
@@ -342,6 +345,7 @@ def plot_2dspikebinstmp(p, s, b, dt, basepath):
         plt.scatter(meanp[0,:],meanp[1,:],c=frs, cmap='jet')
         plt.title('Neuron %i' % i)
 
+    plt.tight_layout()
     filepath = "%s-2dspikebins.png" % basepath
     plt.savefig(filepath, dpi=600, bbox_inches='tight')
 
