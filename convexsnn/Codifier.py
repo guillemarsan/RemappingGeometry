f8d04c47e515ec28bf3c8337d64d8ac273813956
Guillermo Martín Sánchez <guillemartinsan@gmail.com>
2022-05-12 18:10:07 +0100

    Added twisted and 6D tori codifiers

--- convexsnn/Codifier.py
+++ convexsnn/Codifier.py
@@ -1,5 +1,4 @@
 import numpy as np
-import matplotlib.pyplot as plt
 class ProjectionCod():
 
     def codify(self, p, dp, A, Theta):
@@ -30,9 +29,9 @@ class ProjectionCod():
 
         return p, dp
 
-class TorusCod():
+class DonutCod():
 
-    def codify(self, p, dp, A, Theta):
+    def codify(self, p, dp, A, Theta, type='square'):
         dbox = Theta.shape[0] + 1
         time_steps = p.shape[1]
         x = np.zeros((dbox,time_steps))
@@ -48,8 +47,6 @@ class TorusCod():
         x[0,:] = (A + (A/2)*np.cos(p[0,:]))*np.cos(p[1,:])
         x[1,:] = (A + (A/2)*np.cos(p[0,:]))*np.sin(p[1,:])
         x[2,:] = (A/2)*np.sin(p[0,:])
-        ax = plt.axes(projection='3d')
-        #ax.plot3D(x[0,:], x[1,:],x[2,:])
 
         dx[0,:] = (-(A/2)*np.sin(p[0,:])*dp[0,:])*np.cos(p[1,:]) - (A + (A/2)*np.cos(p[0,:]))*np.sin(p[1,:])*dp[1,:]
         dx[1,:] = (-(A/2)*np.sin(p[0,:])*dp[0,:])*np.sin(p[1,:]) + (A + (A/2)*np.cos(p[0,:]))*np.cos(p[1,:])*dp[1,:]
@@ -57,50 +54,79 @@ class TorusCod():
 
         z[0,:] = A*np.cos(p[1,:])
         z[1,:] = A*np.sin(p[1,:])
-        #ax.plot3D(z[0,:], z[1,:], z[2,:])
         dz[0,:] = -A*np.sin(p[1,:])*dp[1,:]
         dz[1,:] = A*np.cos(p[1,:])*dp[1,:]
 
         return x, dx, z, dz
 
-class Torus4DCod():
+class TorusCod():
 
-    def codify(self, p, dp, A, Theta):
+    def codify(self, p, dp, A, Theta, type='square'):
+        
         dbox = Theta.shape[0] + 1
         time_steps = p.shape[1]
         x = np.zeros((dbox,time_steps))
         dx = np.zeros((dbox,time_steps))
 
-        vect1 = np.array([np.sqrt(3)/4,1/4])
-        vect2 = np.array([1/4,np.sqrt(3)/4])
+        if type == 'rhombus':
+            vect1 = np.array([1,0])
+            vect2 = np.array([1/2,np.sqrt(3)/2])
+    
+            pr = np.zeros_like(p)
+            dpr = np.zeros_like(p)
+            pr[0,:] = vect1[0]*p[0,:] + vect2[0]*p[1,:]
+            pr[1,:] = vect1[1]*p[0,:] + vect2[1]*p[1,:]
+            dpr[0,:] = vect1[0]*dp[0,:] + vect2[0]*dp[1,:]
+            dpr[1,:] = vect1[1]*dp[0,:] + vect2[1]*dp[1,:]
+        else:
+            pr = p.copy()
+            dpr = dp.copy()
 
-        pr = np.zeros_like(p)
-        dpr = np.zeros_like(p)
-        pr[0,:] = vect1[0]*p[0,:] + vect2[0]*p[1,:]
-        pr[1,:] = vect1[1]*p[0,:] + vect2[1]*p[1,:]
-        dpr[0,:] = vect1[0]*dp[0,:] + vect2[0]*dp[1,:]
-        dpr[1,:] = vect1[1]*dp[0,:] + vect2[1]*dp[1,:]
-
+        # Scale
+        # TODO Incorporate A (r) and cycles to define scale (a module)
         cycles = 4*np.pi
-
-        prc = np.zeros_like(p)
-        dprc = np.zeros_like(p)
-        prc[0,:] = cycles * (pr[0,:] + 1)
-        prc[1,:] = cycles * (pr[1,:] + 1)
-        dprc[0,:] = cycles * (dpr[0,:] + 1)
-        dprc[1,:] = cycles * (dpr[1,:] + 1)
-
         r = A/np.sqrt(2)
 
-        x[0,:] = r*np.cos(prc[1,:])
-        x[1,:] = r*np.sin(prc[1,:])
-        x[2,:] = r*np.cos(prc[0,:])
-        x[3,:] = r*np.sin(prc[0,:])
-    
-        dx[0,:] = -r*np.sin(prc[1,:])*dprc[1,:]
-        dx[1,:] = r*np.cos(prc[1,:])*dprc[1,:]
-        dx[2,:] = -r*np.sin(prc[0,:])*dprc[0,:]
-        dx[3,:] = r*np.cos(prc[0,:])*dprc[0,:]
+        alpha = np.zeros_like(p)
+        dalpha = np.zeros_like(p)
+        alpha[0,:] = cycles * (pr[0,:] + 1)
+        alpha[1,:] = cycles * (pr[1,:] + 1)
+        dalpha[0,:] = cycles * (dpr[0,:] + 1)
+        dalpha[1,:] = cycles * (dpr[1,:] + 1)
+
+        
+        if type == 'square' or 'twisted' or 'rhombus':
+            twist = 1/2 if type == 'twisted' else 0
+
+            x[0,:] = r*np.cos(alpha[0,:])
+            x[1,:] = r*np.sin(alpha[0,:])
+            x[2,:] = r*np.cos(alpha[1,:] + twist*alpha[0,:])
+            x[3,:] = r*np.sin(alpha[1,:] + twist*alpha[0,:])
+        
+            dx[0,:] = -r*np.sin(alpha[0,:])*dalpha[0,:]
+            dx[1,:] = r*np.cos(alpha[0,:])*dalpha[0,:]
+            dx[2,:] = -r*np.sin(alpha[1,:] + twist*alpha[0,:])*(dalpha[1,:] + twist*dalpha[0,:])
+            dx[3,:] = r*np.cos(alpha[1,:] + twist*alpha[0,:])*(dalpha[1,:] + twist*dalpha[0,:])
+
+        elif type == '6D':
+
+            twist1 = 1/np.sqrt(3)
+            twist2 = -1/np.sqrt(3)
+
+            x[0,:] = r*np.cos(alpha[0,:])
+            x[1,:] = r*np.sin(alpha[0,:])
+            x[2,:] = r*np.cos(alpha[1,:] + twist1*alpha[0,:])
+            x[3,:] = r*np.sin(alpha[1,:] + twist1*alpha[0,:])
+            x[4,:] = r*np.cos(alpha[1,:] + twist2*alpha[0,:])
+            x[5,:] = r*np.sin(alpha[1,:] + twist2*alpha[0,:])
+        
+            dx[0,:] = -r*np.sin(alpha[0,:])*dalpha[0,:]
+            dx[1,:] = r*np.cos(alpha[0,:])*dalpha[0,:]
+            dx[2,:] = -r*np.sin(alpha[1,:] + twist1*alpha[0,:])*(dalpha[1,:] + twist1*dalpha[0,:])
+            dx[3,:] = r*np.cos(alpha[1,:] + twist1*alpha[0,:])*(dalpha[1,:] + twist1*dalpha[0,:])
+            dx[4,:] = -r*np.sin(alpha[1,:] + twist2*alpha[0,:])*(dalpha[1,:] + twist2*dalpha[0,:])
+            dx[5,:] = r*np.cos(alpha[1,:] + twist2*alpha[0,:])*(dalpha[1,:] + twist2*dalpha[0,:])
+
 
         return x, dx
         
