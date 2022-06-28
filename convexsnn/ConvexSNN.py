d5d3b032e4b419954881784912610c0e8bfae1fc
Guillermo Martín Sánchez <guillemartinsan@gmail.com>
2022-06-28 15:12:08 +0100

    Added ALIF simulation

--- convexsnn/ConvexSNN.py
+++ convexsnn/ConvexSNN.py
@@ -34,3 +34,33 @@ class ConvexSNN():
             r[:,i+1] = rtp
         
         return V, s, r
+
+    def simulate_adaptive(self, c, I, V0=0, r0=0, dt=0.001, time_steps=100, a=5):
+        V = np.zeros((self.n,time_steps))
+        s = np.zeros((self.n,time_steps))
+        r = np.zeros((self.n,time_steps))
+
+        V[:,0] = V[:,0] + V0
+        r[:,0] = r[:,0] + r0
+        
+        Vtp = V[:,0]
+        stp = s[:,0]
+        rtp = r[:,0]
+
+        aTtp = np.zeros_like(self.T)
+        for i in range(time_steps-1):
+            Vt = Vtp + self.Om @ stp
+            rt = rtp + stp
+            aTt = aTtp + a*stp
+
+            Vtp = Vt + dt*(-self.lamb*Vt + self.F @ c[:,i] + I[:,i])
+            stp.fill(0)
+            stp[Vtp > aTtp + self.T] = 1
+            aTtp = aTt + dt*(-self.lamb*aTt)
+            rtp = rt + dt*(-self.lamb*rt)
+
+            V[:,i+1] = Vtp
+            s[:,i+1] = stp
+            r[:,i+1] = rtp
+        
+        return V, s, r
