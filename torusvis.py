635fc21324951d79db8ba2fa825a4ef98085b610
Guillermo Martín Sánchez <guillemartinsan@gmail.com>
2023-08-11 23:30:09 +0100

    Improved the code with convex hull and fill

--- torusvis.py
+++ torusvis.py
@@ -2,13 +2,14 @@ import matplotlib.pyplot as plt
 import numpy as np
 import time
 from scipy.stats import ortho_group
+from scipy.spatial import ConvexHull
 from convexsnn.AngleEncoder import AngleEncoder
 from matplotlib import colors
 
 timestr = time.strftime("%Y%m%d-%H%M%S")
 basepath = './out/' + timestr
 color_list = np.array(plt.rcParams['axes.prop_cycle'].by_key()['color'])
-color_list_schematic = np.array(['#ED1C24','#00A651','#2E3192','#FFDE17','#7F3F98'])
+color_list_schematic = np.array(['#ED1C24','#00A651','#2E3192','#FFDE17','#7F3F98']) #RGBYP
 
 def flip(M):
     Mflip = np.zeros_like(M)
@@ -36,7 +37,7 @@ encoding = 'torus'
 gnomonic = True
 Dtype = 'schematic'
 model = 'scn'
-inhibitory = 1
+inhibitory = 2
 out = 2 if encoding == 'semicircle' else (3 if encoding in {'sphere', 'cylinder'} else 4)
 
 if Dtype == 'load':
@@ -125,8 +126,9 @@ else:
     D = D/np.linalg.norm(D, axis=0)
 
 if encoding in {'sphere','cylinder'}: D = flip(D)
-T = np.ones(n)*0.7
-if inhibitory == 2: T[[0,1,3]] = 0.95
+T = np.ones(n)*0.7 if (encoding == 'sphere' or inhibitory in {1,2}) else np.ones(n)*0.8
+if inhibitory == 2 and model == 'scn': T[[0,1,3]] = 1.38
+if inhibitory == 2 and model == 'ff': T = np.ones(n)*1.38
 
 if encoding == 'semicircle':
 
@@ -184,11 +186,11 @@ if encoding == 'semicircle':
     ax[1].spines['right'].set_visible(False)
 
 elif encoding == 'sphere':
-    theta = np.linspace(0,np.pi,100)
+    theta = np.linspace(0,2*np.pi,200)
     x = np.cos(theta)
     y = np.sin(theta)
-    semicircle = np.vstack([x,y])
-    circle = np.hstack([semicircle, np.vstack([semicircle[0,:], -semicircle[1,:]])])
+    circle = np.vstack([x,y])
+    semicircle = circle[:,:int(circle.shape[1]/2)]
 
     np.random.seed(11)
     if Dtype == 'set':
@@ -233,7 +235,7 @@ elif encoding == 'sphere':
     y = np.sin(u) * np.sin(v)
     z = np.cos(v)
 
-    mesh = np.stack([x,y,z], axis=0)
+    mesh = np.stack([x,y,z], axis=0).reshape(3,-1)
 
     mesh = flip(mesh)
     traj1 = flip(traj1)
@@ -242,13 +244,13 @@ elif encoding == 'sphere':
     
     meshn = mesh.copy()
     meshs = mesh.copy()
-    south = np.argwhere(mesh[2,:,:] < 0)
-    north = np.argwhere(mesh[2,:,:] > 0)
-    meshn[:,south[:,0],south[:,1]] = np.nan
-    meshs[:,north[:,0],north[:,1]] = np.nan
+    south = np.argwhere(mesh[2,:] < 0)
+    north = np.argwhere(mesh[2,:] > 0)
+    meshn[:,south] = np.nan
+    meshs[:,north] = np.nan
     if gnomonic:
-        xgnon, ygnon = gnomonic_proj(meshn[0,:,:], meshn[1,:,:], meshn[2,:,:])
-        xgnos, ygnos = gnomonic_proj(meshs[0,:,:], meshs[1,:,:], meshs[2,:,:])
+        xgnon, ygnon = gnomonic_proj(meshn[0,:], meshn[1,:], meshn[2,:])
+        xgnos, ygnos = gnomonic_proj(meshs[0,:], meshs[1,:], meshs[2,:])
 
     
     # meshn = mesh
@@ -261,15 +263,17 @@ elif encoding == 'sphere':
     # rn = np.tensordot(D.T, meshn,axes=1)
     # rs = np.tensordot(D.T, meshs,axes=1)
 
-    r = np.tensordot(D.T, mesh,axes=1) - T[:,np.newaxis,np.newaxis]
-    rn = np.tensordot(D.T, meshn,axes=1) - T[:,np.newaxis,np.newaxis]
-    rs = np.tensordot(D.T, meshs,axes=1) - T[:,np.newaxis,np.newaxis]
+    r = np.tensordot(D.T, mesh,axes=1) - T[:,np.newaxis]
+    rn = np.tensordot(D.T, meshn,axes=1) - T[:,np.newaxis]
+    rs = np.tensordot(D.T, meshs,axes=1) - T[:,np.newaxis]
 
-    r1 = np.tensordot(D.T, traj1, axes=1) - T[:,np.newaxis,np.newaxis]
-    r2 = np.tensordot(D.T,traj2, axes=1) - T[:,np.newaxis,np.newaxis]
+    r1 = np.tensordot(D.T, traj1, axes=1) - T[:,np.newaxis]
+    r2 = np.tensordot(D.T,traj2, axes=1) - T[:,np.newaxis]
 
     
     maxr = np.argmax(r,axis=0)
+    maxrn = np.argmax(rn,axis=0)
+    maxrs = np.argmax(rs,axis=0)
     maxr1 = np.argmax(r1,axis=0)
     maxr2 = np.argmax(r2,axis=0)
 
@@ -289,68 +293,98 @@ elif encoding == 'sphere':
     
     ax[0,0].quiver(zeros,zeros,zeros,D[0,:],D[1,:],D[2,:], color=color_list, arrow_length_ratio=0)
 
-    for i in np.arange(n):
+    for i in np.arange(n-1,-1,-1):
 
         cmap = colors.ListedColormap(['white', color_list[i%10]])
         bounds=[0,0,1]
         norm = colors.BoundaryNorm(bounds, cmap.N)
 
         meshi = mesh.copy()
-        inactiveidxn = np.argwhere(r[i,:,:] < 0)
-        meshi[:,inactiveidxn[:,0],inactiveidxn[:,1]] = np.nan
+        inactiveidxn = np.argwhere(r[i,:] < 0)
+        meshi[:,inactiveidxn] = np.nan
 
-        inactiveidxn = np.argwhere(rn[i,:,:] < 0)
-        inactiveidxs = np.argwhere(rs[i,:,:] < 0)
+        inactiveidxn = np.argwhere(rn[i,:] < 0)
+        inactiveidxs = np.argwhere(rs[i,:] < 0)
         if not gnomonic:
             meshni = meshn.copy()
             meshsi = meshs.copy()
-            meshni[:,inactiveidxn[:,0],inactiveidxn[:,1]] = np.nan
-            meshsi[:,inactiveidxs[:,0],inactiveidxs[:,1]] = np.nan
+            meshni[:,inactiveidxn] = np.nan
+            meshsi[:,inactiveidxs] = np.nan
         else:
             xgnoni = xgnon.copy()
             ygnoni = ygnon.copy()
             xgnosi = xgnos.copy()
             ygnosi = ygnos.copy()
 
-            xgnoni[inactiveidxn[:,0], inactiveidxn[:,1]] = np.nan
-            xgnosi[inactiveidxs[:,0], inactiveidxs[:,1]] = np.nan
-            ygnoni[inactiveidxn[:,0], inactiveidxn[:,1]] = np.nan
-            ygnosi[inactiveidxs[:,0], inactiveidxs[:,1]] = np.nan
+            xgnoni[inactiveidxn] = np.nan
+            xgnosi[inactiveidxs] = np.nan
+            ygnoni[inactiveidxn] = np.nan
+            ygnosi[inactiveidxs] = np.nan
 
 
         r1[i,r1[i,:]<0] = 0
         r2[i,r2[i,:]<0] = 0
 
+        alpha = 0.5
         if model == 'scn':
             notmax = np.argwhere(maxr != i)
-            meshi[:,notmax[:,0],notmax[:,1]] = np.nan    
+            meshi[:,notmax] = np.nan    
+
+            notmaxn = np.argwhere(maxrn != i)
+            notmaxs = np.argwhere(maxrs != i)
+
+            if not gnomonic:
+                meshni[:,notmaxn] = np.nan
+                meshsi[:,notmaxs] = np.nan
+            else:
+                xgnoni[notmaxn] = np.nan
+                xgnosi[notmaxs] = np.nan
+                ygnoni[notmaxn] = np.nan
+                ygnosi[notmaxs] = np.nan
 
             r1[i,maxr1 != i] = 0
             r2[i,maxr2 != i] = 0
-        
-        ax[0,0].plot_surface(meshi[0,:,:],meshi[1,:,:],meshi[2,:,:], alpha=0.4, color=color_list[i%10])
 
-        if model == 'ff':
-            # ax[0,1].pcolormesh(meshin[0,:,:], meshin[1,:,:], meshin[2,:,:], alpha=0.5, cmap=cmap)
-            # ax[0,2].pcolormesh(meshis[0,:,:], meshis[1,:,:], meshis[2,:,:], alpha=0.5, cmap=cmap)
-            if not gnomonic:
-                ax[0,1].scatter(meshni[1,:,:], meshni[0,:,:], s = 1.5, alpha=0.2, color=color_list[i%10])
-                ax[0,2].scatter(meshsi[1,:,:], meshsi[0,:,:], s = 1.5, alpha=0.2, color=color_list[i%10])
-            else:
-                ax[0,1].scatter(ygnoni, xgnoni, s = 1.7, alpha=0.2, color=color_list[i%10])
-                ax[0,2].scatter(ygnosi, xgnosi, s = 1.7, alpha=0.2, color=color_list[i%10])
+            alpha = 0.8
+        
+        meshisq = meshi.reshape(3,200,200)
+        ax[0,0].plot_surface(meshisq[0,:,:],meshisq[1,:,:],meshisq[2,:,:], alpha=alpha, color=color_list[i%10])
+
+       
+        if not gnomonic:
+            pointsni = meshni[:2, np.logical_not(np.isnan(meshni[0,:]))].T
+            if len(pointsni)>0:
+                hullni = ConvexHull(pointsni)
+                ax[0,1].fill(pointsni[hullni.vertices,1], pointsni[hullni.vertices,0], alpha=0.5, color=color_list[i%10], lw=0)
+            pointssi = meshsi[:2, np.logical_not(np.isnan(meshsi[0,:]))].T
+            if len(pointssi)>0:
+                hullsi = ConvexHull(pointssi)
+                ax[0,2].fill(pointssi[hullsi.vertices,1], pointssi[hullsi.vertices,0], alpha=0.5, color=color_list[i%10], lw=0)
+        else:
+            gnoni = np.vstack([xgnoni, ygnoni])
+            pointsni = gnoni[:, np.logical_not(np.isnan(gnoni[0,:]))].T
+            pointsni = pointsni[np.linalg.norm(pointsni, axis=1)<3,:]
+            if len(pointsni)>0:
+                hullni = ConvexHull(pointsni)
+                ax[0,1].fill(pointsni[hullni.vertices,1], pointsni[hullni.vertices,0], alpha=0.5, color=color_list[i%10], lw=0)
+            gnosi = np.vstack([xgnosi, ygnosi])
+            pointssi = gnosi[:, np.logical_not(np.isnan(gnosi[0,:]))].T
+            pointssi = pointssi[np.linalg.norm(pointssi, axis=1)<3,:]
+            if len(pointssi)>0:
+                hullsi = ConvexHull(pointssi)
+                ax[0,2].fill(pointssi[hullsi.vertices,1], pointssi[hullsi.vertices,0], alpha=0.5, color=color_list[i%10], lw=0)
 
         ax1.plot(np.linspace(-1,1,100),r1[i,:], color=color_list[i%10])
         ax2.plot(np.linspace(-1,1,100),r2[i,:], color=color_list[i%10])
 
     
-    if model == 'scn':
-        if not gnomonic:
-            ax[0,1].scatter(meshn[1,:,:].reshape(-1,),meshn[0,:,:].reshape(-1,), s=1.2, c=color_list[np.argmax(rn,axis=0).reshape(-1,).astype(int) % 10])
-            ax[0,2].scatter(meshs[1,:,:].reshape(-1,),meshs[0,:,:].reshape(-1,), s=1.2, c=color_list[np.argmax(rs,axis=0).reshape(-1,).astype(int) % 10])
-        else:
-            ax[0,1].scatter(ygnon.reshape(-1,),xgnon.reshape(-1,), s=2, c=color_list[np.argmax(rn,axis=0).reshape(-1,).astype(int) % 10])
-            ax[0,2].scatter(ygnos.reshape(-1,),xgnos.reshape(-1,), s=2, c=color_list[np.argmax(rs,axis=0).reshape(-1,).astype(int) % 10])
+    # if model == 'scn':
+    #     if not gnomonic:
+    #         ax[0,1].scatter(meshn[1,:,:].reshape(-1,),meshn[0,:,:].reshape(-1,), s=1.2, c=color_list[np.argmax(rn,axis=0).reshape(-1,).astype(int) % 10])
+    #         ax[0,2].scatter(meshs[1,:,:].reshape(-1,),meshs[0,:,:].reshape(-1,), s=1.2, c=color_list[np.argmax(rs,axis=0).reshape(-1,).astype(int) % 10])
+    #     else:
+    #         ax[0,1].scatter(ygnon.reshape(-1,),xgnon.reshape(-1,), s=2, c=color_list[np.argmax(rn,axis=0).reshape(-1,).astype(int) % 10])
+    #         ax[0,2].scatter(ygnos.reshape(-1,),xgnos.reshape(-1,), s=2, c=color_list[np.argmax(rs,axis=0).reshape(-1,).astype(int) % 10])
 
     if trajs:
         ax[0,0].plot(traj1[0,:],traj1[1,:],traj1[2,:],c='k',zorder=10)
@@ -423,7 +457,7 @@ elif encoding == 'sphere':
     ax2.spines['top'].set_visible(False)
     ax2.spines['right'].set_visible(False)
 
-
+# TODO with plt.fill
 elif encoding == 'cylinder':
 
     points = np.linspace(-1,1,100)
@@ -590,27 +624,33 @@ else:
     u = np.mgrid[0:np.pi:200j]
     x = np.cos(u)
     y = np.sin(u)
+    semicircle = np.stack([x,y], axis=0)
+
+    mesh = np.mgrid[-1:1:200j, -1:1:200j].reshape(2,-1)
 
     xaxis = np.linspace(-0.9,0.9,100)
 
     A = 1/2 if inhibitory == 0 else 0.15
     f = 2
-    path1p = np.vstack([xaxis,np.zeros_like(xaxis)-0.5]) if inhibitory == 0 else np.vstack([xaxis,-A*np.sin(f*xaxis)])
-    path2p = np.vstack([xaxis,np.zeros_like(xaxis)+0.5])
+    path1p = np.vstack([xaxis,np.zeros_like(xaxis)-0.55]) if inhibitory == 0 else np.vstack([xaxis,-A*np.sin(f*xaxis)])
+    path2p = np.vstack([xaxis,np.zeros_like(xaxis)+0.55])
 
     path1f = np.vstack([xaxis,A*np.sin(f*xaxis)-0.4])
     path2f = np.vstack([xaxis,A*np.cos(f*xaxis)+0.3])
 
+    meshtorus = Encoder.encode(mesh)
     traj1p = Encoder.encode(path1p)
     traj2p = Encoder.encode(path2p)
     traj1f = Encoder.encode(path1f)
     traj2f = Encoder.encode(path2f)
 
+    r = D.T @ meshtorus - T[:,np.newaxis]
     r1p = D.T @ traj1p - T[:,np.newaxis]
     r2p = D.T @ traj2p - T[:,np.newaxis]
     r1f = D.T @ traj1f - T[:,np.newaxis]
     r2f = D.T @ traj2f - T[:,np.newaxis]
 
+    maxr = np.argmax(r, axis=0)
     maxr1p = np.argmax(r1p,axis=0)
     maxr2p = np.argmax(r2p,axis=0)
     maxr1f = np.argmax(r1f,axis=0)
@@ -619,11 +659,17 @@ else:
     
     plt.figure() 
     
-    _, ax = plt.subplots(2,4)
+    fig, ax = plt.subplots(3,4)
+    for i in np.arange(2):
+        for j in np.arange(4):
+            ax[i+1,j].remove()
+    ax1 = fig.add_subplot(3,2,3)
+    ax3 = fig.add_subplot(3,2,4)
+    ax2 = fig.add_subplot(3,2,5)
+    ax4 = fig.add_subplot(3,2,6)
     if Dtype == 'schematic': color_list = color_list_schematic
 
     zeros = np.zeros(n,)
-    semicircle = np.stack([x,y], axis=0)
     ax[0,0].quiver(zeros,zeros,D[0,:],D[1,:], color=color_list, angles='xy', scale_units='xy', scale=1, width=0.02)
     ax[0,0].plot(semicircle[0,:],semicircle[1,:],linewidth=1,c='k',linestyle=':')
     ax[0,0].set_aspect('equal')
@@ -642,9 +688,8 @@ else:
 
     for i in np.arange(n):
 
-        pointsi = r[i,:].reshape((100,100))
-        pointsi = np.flipud(pointsi)
-        inactiveidx = np.argwhere(pointsi < 0)
+        pointsi = mesh.copy()
+        activeidx = np.argwhere(r[i,:] > 0)[:,0]
         
         # For graded plotting
         # pointsi[inactiveidx[:,0],inactiveidx[:,1]] = 0
@@ -656,33 +701,41 @@ else:
         bounds=[0,0,1]
         norm = colors.BoundaryNorm(bounds, cmap.N)
 
-        balpha = 0.5
-        alphas = np.ones((100,100))*balpha
-        alphas[inactiveidx[:,0],inactiveidx[:,1]] = 0
+        # balpha = 0.5
+        # alphas = np.ones((100,100))*balpha
+        # alphas[inactiveidx[:,0],inactiveidx[:,1]] = 0
 
         r1p[i,r1p[i,:]<0] = 0
         r2p[i,r2p[i,:]<0] = 0
         r1f[i,r1f[i,:]<0] = 0
         r2f[i,r2f[i,:]<0] = 0
         
-        
+        alpha = 0.5
         if model == 'scn':
-            mask = maxs == i
-            mask = np.flipud(mask.reshape((100,100)))
-            alphas = alphas*mask*(1/balpha)
+            # mask = maxs == i
+            # mask = np.flipud(mask.reshape((100,100)))
+            # alphas = alphas*mask*(1/balpha)
+            activeidx = np.intersect1d(activeidx, np.argwhere(maxr == i)[:,0])
+            alpha = 0.8
 
             r1p[i,maxr1p != i] = 0
             r2p[i,maxr2p != i] = 0
             r1f[i,maxr1f != i] = 0
             r2f[i,maxr2f != i] = 0
-       
-        ax[0,2].imshow(pointsi, extent=[-1,1,-1,1], alpha=alphas, cmap=cmap, norm=norm)
-        ax[0,3].imshow(pointsi, extent=[-1,1,-1,1], alpha=alphas, cmap=cmap, norm=norm)
 
-        ax[1,0].plot(xaxis,r1p[i,:], color=color_list[i%10])
-        ax[1,1].plot(xaxis,r2p[i,:], color=color_list[i%10])
-        ax[1,2].plot(xaxis,r1f[i,:], color=color_list[i%10])
-        ax[1,3].plot(xaxis,r2f[i,:], color=color_list[i%10])
+        pointsi = pointsi[:, activeidx]
+        pointsi = pointsi.T
+        if len(pointsi)>0:
+            hullni = ConvexHull(pointsi)
+            ax[0,2].fill(pointsi[hullni.vertices,0], pointsi[hullni.vertices,1], alpha=alpha, color=color_list[i%10], lw=0)
+            ax[0,3].fill(pointsi[hullni.vertices,0], pointsi[hullni.vertices,1], alpha=alpha, color=color_list[i%10], lw=0)
+        # ax[0,2].imshow(pointsi, extent=[-1,1,-1,1], alpha=alphas, cmap=cmap, norm=norm)
+        # ax[0,3].imshow(pointsi, extent=[-1,1,-1,1], alpha=alphas, cmap=cmap, norm=norm)
+
+        ax1.plot(xaxis,r1p[i,:], color=color_list[i%10])
+        ax2.plot(xaxis,r2p[i,:], color=color_list[i%10])
+        ax3.plot(xaxis,r1f[i,:], color=color_list[i%10])
+        ax4.plot(xaxis,r2f[i,:], color=color_list[i%10])
     
     if trajs:
         ax[0,2].plot(path1p[0,:], path1p[1,:], linewidth=1, c='k')
@@ -694,14 +747,16 @@ else:
     maxt = np.max([r1p,r2p,r1f,r2f])
     ax[0,2].set_xlim([-1,1])
     ax[0,2].set_ylim([-1,1])
+    ax[0,2].set_aspect('equal')
     ax[0,3].set_xlim([-1,1])
     ax[0,3].set_ylim([-1,1])
-    [ax[1,i].set_ylim([0,maxt+0.05]) for i in np.arange(4)]
-    [ax[1,i].set_xlim([-1,1]) for i in np.arange(4)]
-    [ax[1,i].set_aspect(3) for i in np.arange(4)]
-    [ax[1,i].spines['top'].set_visible(False) for i in np.arange(4)]
-    [ax[1,i].spines['right'].set_visible(False) for i in np.arange(4)]
+    ax[0,3].set_aspect('equal')
+    [axvar.set_ylim([0,maxt+0.05]) for axvar in {ax1,ax2,ax3,ax4}]
+    [axvar.set_xlim([-1,1]) for axvar in {ax1,ax2,ax3,ax4}]
+    #[axvar.set_aspect(3) for axvar in {ax1,ax2,ax3,ax4}]
+    [axvar.spines['top'].set_visible(False) for axvar in {ax1,ax2,ax3,ax4}]
+    [axvar.spines['right'].set_visible(False) for axvar in {ax1,ax2,ax3,ax4}]
 
 plt.tight_layout()
-filepath = "{0}_torusvis.png".format(basepath)
+filepath = "{0}_torusvis.svg".format(basepath)
 plt.savefig(filepath, dpi=600, bbox_inches='tight')
