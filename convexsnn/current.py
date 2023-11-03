40200e7f41027fba14ad24e3db6956a9e5a0f08f
Guillermo Martín Sánchez <guillemartinsan@gmail.com>
2023-11-03 12:19:41 +0000

    added non-random current

--- convexsnn/current.py
+++ convexsnn/current.py
@@ -8,7 +8,7 @@ def get_noise(dbbox, t, G, noise_amp, noise_seed=0):
     I = G @ b
     return I,b
 
-def get_current(n, t, current_per, current_amp, current_seed=0):
+def get_current_random(n, t, current_per, current_amp, current_seed=0):
 
     # Current manipulation
     start = 0 #int(t.shape[0]/5)
@@ -19,4 +19,13 @@ def get_current(n, t, current_per, current_amp, current_seed=0):
 
     current = np.zeros((n,t.shape[0]))
     current[idx,start:] = current_amp
+    return current
+
+def get_current(n, t, tagged_idx, current_amp):
+
+    # Current manipulation
+    start = 0 
+    
+    current = np.zeros((n,t.shape[0]))
+    current[tagged_idx,start:] = current_amp
     return current
