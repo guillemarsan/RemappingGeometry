63a10e27f6f282a183ab576ac4adb50522d6b068
Guillermo Martín Sánchez <guillemartinsan@gmail.com>
2024-02-09 13:57:22 +0000

    Fixed non orthogonal embeddings with sigma != -1

--- convexsnn/embedding.py
+++ convexsnn/embedding.py
@@ -1,17 +1,18 @@
 import numpy as np
-from scipy.stats import ortho_group
+# from scipy.stats import ortho_group
 
-def get_embedding(dbbox, dinput, env, variance=-1):
+def get_embedding(dbbox, dinput, env, ortho=True, variance=-1):
 
     np.random.seed(env)
     if variance != -1:
-        noise = np.random.normal(0, variance, (dbbox,dinput))
-        Q = np.eye(dbbox)
-        Basis = Q[:,:dinput] + noise
+        Q = np.eye(dbbox)[:,:dinput]
+        Basis = np.random.normal(Q, variance, (dbbox,dinput))
     else:
-        Q = ortho_group.rvs(dbbox)
-        Basis = Q[:,:dinput]
-
-    Theta = Basis/np.linalg.norm(Basis,axis=0)
-
+        Basis = np.random.uniform(size=(dbbox,dinput))
+        
+    aux = np.linalg.qr(Basis)[0]
+    Theta = np.zeros_like(aux)
+    for i in np.arange(aux.shape[1]):
+        Theta[:,i] = np.sign(aux[0,i])*np.sign(Basis[0,i])*aux[:,i]
+   
     return Theta
